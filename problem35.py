6890769b1318f3706e7422afb3bde1508ff24436
hatsune_kinoshita <hatsune.k.pc8010@gmail.com>
2025-07-22 23:27:54 -0700

    35 途中

--- problem35.py
+++ problem35.py
@@ -1,25 +1,36 @@
-def is_prime(n):
-    if n < 2:
-        return False
-    if n == 2:
-        return True
-    elif n % 2 == 0:
-        return False
-    i = 3
-    while i <= n ** 0.5:
-        if n % i == 0:
-            return False
-        i +=2 
-    return True
+# {/* <p>The sum of the primes below $10$ is $2 + 3 + 5 + 7 = 17$.</p>
+# <p>Find the sum of all the primes below two million.</p> */}
 
-list_test = []
-for i in range(1,101):
-    if is_prime(i):
-        list_test.append(i)
+# function SummationOfPrimes(limit) {
+#     // 1. 最初は、すべての数を「素数」だと思う（true）
+#     let isPrime = new Array(limit).fill(true);
+#     //無限
+    
+#     // 0と1は素数ではないから false にする
+#     isPrime[0] = false;
+#     isPrime[1] = false;
 
-def is_circular(n):
-    if n < 10:
-        return n
-    n_str = str(n)
-    for j in n_str:
-        new_n = j
+#     // 2からスタートして、素数じゃないものを消す
+#     for (let i = 2; i * i < limit; i++) {
+#         // もしiが素数なら（まだtrueのままなら）
+#         if (isPrime[i]) {
+#             // iの倍数は素数じゃないから false にする
+#             for (let j = i * i; j < limit; j += i) {
+#                 isPrime[j] = false;
+#             }
+#         }
+#     }
+
+#     // 2. 残ったtrueの場所だけ合計する
+#     let sum = 0;
+#     for (let i = 2; i < limit; i++) {
+#         if (isPrime[i]) {
+#             sum += i;
+#         }
+#     }
+
+#     return sum;
+# }
+
+# // 実行してみる
+# console.log(SummationOfPrimes(2000000));
