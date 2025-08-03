f3735fcd74ab775c14f99d6fb74684e867e68bab
hatsune_kinoshita <hatsune.k.pc8010@gmail.com>
2025-08-03 14:43:00 -0700

    35 done

--- problem35.py
+++ problem35.py
@@ -1,6 +1,8 @@
 # {/* <p>The sum of the primes below $10$ is $2 + 3 + 5 + 7 = 17$.</p>
 # <p>Find the sum of all the primes below two million.</p> */}
-def summation_of_primes(n):
+##エラトネスのふるい
+## sieve of eratosthenes
+def sieve_of_eratosthenes(n):
     a = [True] * (n +1)
     a[0] = a[1] = False
     for i in range(2, int(n ** 0.5) + 1):
@@ -10,19 +12,29 @@ def summation_of_primes(n):
             end = n + 1
             for j in range(start, end, step):
                 a[j] = False
-    r =[]
-    for idx in range(len(a)):
-        if a[idx]:
-            r.append(idx)
-    return r
+    return a
 
-rotation = set()
+def is_circular_prime(n):
+    num = str(n)
+    for j in range(len(num)):
+        if not is_prime[int(num)]:
+            return False
+        num = num[1:] + num[0]
+    return True
 
-for k in range(10,100):
-    k_str = str(k)
-    num = [k[j:]+k[:j] for j in range(len(k_str))]
-    rotation.add(num)
+def main():
+    MAX = 1000000
+    global is_prime
+    is_prime = sieve_of_eratosthenes(MAX)
+    count = 0
+    result = []
+    for i in range(2,MAX):
+        if is_circular_prime(i):
+            result.append(i)
+            count += 1
+    print(count)
+    print(result)
 
-print(rotation)
+main()
 
 
