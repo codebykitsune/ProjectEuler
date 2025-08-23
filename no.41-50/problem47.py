320a090acdee9b24e4cefe5e8626720b2d55a7a8
hatsune_kinoshita <hatsune.k.pc8010@gmail.com>
2025-08-22 23:16:58 -0700

    48 done

--- no.41-50/problem47.py
+++ no.41-50/problem47.py
@@ -1,4 +1,5 @@
 def factorize(n): 
+    # 素因数と指数を格納する辞書
     list_of_primes = {}
     for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
@@ -17,9 +18,11 @@ c = 4
 for i in range(2,n):
     flag = True
     for j in range(c):
+        #c回連続する数それぞれがcつの異なる素因数を持つかどうか
         length = len(factorize(i+j))
         if length != c:
             flag = False
+    # cつの数全てが条件を満たしていたらTrue
     if flag == True:
         print(i)
         break
